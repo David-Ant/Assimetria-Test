105a87647d3537264de39f126ce6fc7628252abf
David Ant <145033515+David-Ant@users.noreply.github.com>
2025-12-07 20:30:06 +0000

    connected backend to postgres db with prisma

--- backend/src/routes/articleRoutes.js
+++ backend/src/routes/articleRoutes.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client/client.js";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
 
 const articleRoutes = Router();
+const prisma = new PrismaClient({adapter});
 
 // Temporary placeholder methods
 articleRoutes.get("/", (req, res) => {
