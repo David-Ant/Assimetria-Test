d7714564554a23ed33964520b607dfa224dbd24b
David Ant <145033515+David-Ant@users.noreply.github.com>
2025-12-10 15:45:21 +0000

    updated articlejob to insert directly to db with prisma

--- backend/src/services/articleJob.js
+++ backend/src/services/articleJob.js
@@ -1,4 +1,12 @@
 import callAI from "./aiClient.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client/client.js";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({adapter});
 
 const generateArticle = async () => {
     try {
@@ -26,18 +34,13 @@ const generateArticle = async () => {
 
 const postArticle = async (title, body) => {
     try {
-        const response = await fetch(`http://localhost:4000/articles`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title, content: body }),
+        const article = await prisma.article.create({
+            data: {
+                title: title,
+                content: body,
+            },
         });
-
-        if (!response.ok) {
-            throw new Error(`Failed to post article: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        return data;
+        return article;
     } catch (error) {
         console.error("Error posting article:", error);
         throw error;
